06807816f9ff1afaef27cbc9933e9702ac8d2a9c
rishitha  bobba <rb79f@mail.umkc.edu>
2018-11-30 22:08:53 -0600

    update

--- SourceCode/FixItUpApp/app/src/main/java/com/fixitup/cs5551/fixitupapp/Rating.java
+++ SourceCode/FixItUpApp/app/src/main/java/com/fixitup/cs5551/fixitupapp/Rating.java
@@ -4,66 +4,50 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class Rating extends AppCompatActivity{
+public class Rating extends AppCompatActivity {
     RatingBar mRating;
-    Button mLogin;
-
+    Button mButton;
+    private DatabaseReference mDatabase,mDemo;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener firebaseAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rating);
-        //mAuth = FirebaseAuth.getInstance();
-       // firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
-            //@Override
-           // public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser(); //get the information of the current user
-            //    if (user != null){
-                    //if not null, move to another activity, to be created later.
-                    //Remember the current context!
-                //    Intent intent = new Intent(Rating.this, Rating.class);
-                //    intent.putExtra("user_id", user.getUid());
-                //    intent.putExtra("user_email", user.getEmail());
-                //    startActivity(intent);
-                    finish();
-                //    return;
-             //   }
-           // }
-        //};
-        mRating = (RatingBar) findViewById(R.id.Ratingbar);
-        mLogin = (Button) findViewById(R.id.submit);
+        setContentView(R.layout.activity_main);
+        mRating = (RatingBar) findViewById(R.id.ratingBar);
+        mButton = (Button) findViewById(R.id.button);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDemo = mDatabase.child("rating");
+
+
         mRating.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                Toast.makeText(Rating.this,"stars" +(int)rating, Toast.LENGTH_LONG).show();
+                Toast.makeText(Rating.this,"stars:" + rating,Toast.LENGTH_SHORT).show();
             }
         });
-        mLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(Rating.this,"stars:" + mRating.getRating(),Toast.LENGTH_SHORT).show();
+        mButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view)
+            {
+                Toast.makeText(Rating.this,"stars:"+ mRating.getRating(),Toast.LENGTH_SHORT).show();
+                float value = Float.valueOf(mRating.getRating());
+                mDemo.push().setValue(value);
             }
         });
-       /* @Override
-        protected void onStart() {
-            super.onStart();
-            mAuth.addAuthStateListener(firebaseAuthListener);
-        }
 
-        @Override
-        protected void onStop() {
-            super.onStop();
-            mAuth.removeAuthStateListener(firebaseAuthListener);
-        }*/
-
+    }
 }
